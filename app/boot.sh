e3ba5b1ec2b5a7a923aae0ea9fea837cb5f4c314
Lucas <lucassrod@gmail.com>
2020-07-26 00:06:48 -0300

    Test with boot.sh

--- app/boot.sh
+++ app/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dockerize -template .env.template:.env -wait tcp://db:3306 -timeout 40s
+# dockerize -template .env.template:.env -wait tcp://db:3306 -timeout 40s
 composer install
 php artisan key:generate
 php artisan config:cache
