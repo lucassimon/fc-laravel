d22c2ecff2f367a548e698ddf13c8db0f1ad5dc4
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2020-07-26 22:40:42 -0300

    Uppercase PhpUnit (#1)
    * Fix the filename
    
    * Adds docker ps
    
    * runs composer
    
    * Change DockerFile build
    
    * copy .env
    
    * Runs migrate and phpunit
    
    * ajustando dockerfile
    
    * try to run dk compose
    
    * Test with boot.sh
    
    * debugger
    
    * Set permission on www folder
    
    * Copy .env
    
    * Copy .env
    
    * adds chmod
    
    * CHMOD
    
    * Comment chmod
    
    * Runs exec with root user
    
    * Runs php artisan ...
    
    * Rodando o composer install
    
    * Fix command phpunit
    
    * Uppercase PhpUnit

--- app/boot.sh
+++ app/boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dockerize -template .env.template:.env -wait tcp://db:3306 -timeout 40s
+# dockerize -template .env.template:.env -wait tcp://db:3306 -timeout 40s
+cp .env.example .env
 composer install
 php artisan key:generate
 php artisan config:cache
